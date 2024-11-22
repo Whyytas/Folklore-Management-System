9bd0b8219644576fc43091c57cc1a203d1ed2daa
Whyytas <vitas.ilekis@ktu.edu>
2024-11-22 10:21:14 +0200

    added requirements

--- FolkloreManagementSystem/settings.py
+++ FolkloreManagementSystem/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-+_n6j3p7xr&pcxd1ujofrbs!k6hm^=ijg*6t#qgd5dm$_9*j50
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://folkloremanagement.azurewebsites.net/']
+ALLOWED_HOSTS = ['http://folkloremanagement.azurewebsites.net/',
+                 'folkloremanagement.azurewebsites.net',
+                 'https://folkloremanagement.azurewebsites.net/',]
 
 
 # Application definition
