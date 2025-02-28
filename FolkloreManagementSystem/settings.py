36d807112d0d79f92d74479b45259da864439c4c
Whyytas <vitas.ilekis@ktu.edu>
2025-02-28 12:21:13 +0200

    Templates Fix 1

--- FolkloreManagementSystem/settings.py
+++ FolkloreManagementSystem/settings.py
@@ -117,7 +117,7 @@ ROOT_URLCONF = 'FolkloreManagementSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': ['templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
