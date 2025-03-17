9c714750f589ba034846eefc05fa18a9c5438e84
Whyytas <vitas.ilekis@ktu.edu>
2025-03-17 15:32:23 +0200

    Main page cards sizing fix. Ansambliai choosing fix

--- mainPage/views.py
+++ mainPage/views.py
@@ -1,23 +1,28 @@
+from itertools import chain
+
 from django.shortcuts import render
 from django.utils.timezone import now
-from itertools import chain
-from operator import attrgetter
 
+from Ansambliai.models import Ansamblis
+from Instrumentai.models import Instrumentas
+from Kuriniai.models import Kurinys
 from Renginiai.models import Renginys
 from Repeticijos.models import Repeticija
-from Kuriniai.models import Kurinys
-from Instrumentai.models import Instrumentas
+
 
 def main_page(request):
     ansamblis_id = request.GET.get('ansamblis_id')
+    all_ansambliai = Ansamblis.objects.all()
 
     renginiai_qs = Renginys.objects.filter(data_laikas__gte=now())
     repeticijos_qs = Repeticija.objects.filter(data__gte=now())
     instrumentai_qs = Instrumentas.objects.all()
+    kuriniai_qs = Kurinys.objects.all()
 
     if ansamblis_id:
         renginiai_qs = renginiai_qs.filter(ansamblis_id=ansamblis_id)
         instrumentai_qs = instrumentai_qs.filter(ansamblis_id=ansamblis_id)
+        kuriniai_qs = kuriniai_qs.filter(ansambliai__id=ansamblis_id)
 
     renginiai = renginiai_qs.order_by('data_laikas')[:5]
     repeticijos = repeticijos_qs.order_by('data')[:5]
@@ -27,12 +32,13 @@ def main_page(request):
         key=lambda e: getattr(e, 'data_laikas', getattr(e, 'data', None))
     )[:5]
 
-    newest_kuriniai = Kurinys.objects.order_by('-id')[:5]
+    newest_kuriniai = kuriniai_qs.order_by('-created_at').distinct()[:5]
     newest_instrumentai = instrumentai_qs.order_by('-id')[:5]
 
     context = {
         'events': events_combined,
         'kuriniai': newest_kuriniai,
         'instrumentai': newest_instrumentai,
+        'all_ansambliai': all_ansambliai,
     }
     return render(request, 'main.html', context)
